04754ae867967d8aa8f0e392502ee43502fff1dd
Brian Kerama <keramabrianmomanyi@gmail.com>
2018-03-27 08:36:26 +0300

    add initial page structure

---
+++ js/scripts.js
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+  <link href="css/styles.css" rel="stylesheet" type="text/css">
+  <script src="js/jquery-3.3.1.js"></script>
+  <script src="js/scripts.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <title>ANITA's KITCHEN</title>
+    <body>
+  <div class="landing">
+    <img src="img/assets/backgrounds/landing.jpg" height="636px" alt="A landing photo">
+    <br><br><br>
+    <span class="logo">
+      <img src="img/assets/logoBig.png">
+  </div>
+  <div class="utra">Happy hour Party Time
+    <img src="img/assets/foodtop/breakfast.jpg" width="385px" height="200px"><img src="img/assets/foodtop/lunch.jpg" width="381px" height="200px"><img src="img/assets/foodtop/dinner.jpg" width="385px" height="200px">
+  </div>
+   <div class="column">
+    <img src="img/assets/fastFood1.jpg">
+    <div class="wells">
+    <img src="img/assets/fastFood2.jpg">
+    <h1>Fast Food</h1><subtitle>You'll love our delicious fast food</subtitle>
+      <p>Lorem ipsum dolor sit amet,consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua,Ut enimad minimveniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duisaute irure dolor in re prehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Exeteur sint occaecat cupidatat non proident</p>
+    <img src="img/assets/drink1.jpg" height="500px" width="500px">
+    <h2>Drinks</h2>
+      <subtitle>We also produce delicious drinks</subtitle>
+      <p>Lorem ipsum dolor sit amet,consectetur adipisicing elit,sed do eiusmod tempor incididunt ut labore et dolore magna aliqua,Ut enimad minimveniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Duisaute irure dolor in re prehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Exeteur sint occaecat cupidatat non proident</p>
+</div>
+<div class=("jumbotron")"container">
+  <img src="img/assets/backgrounds/foodsBg.jpg"><img src="img/assets/fastfood/chickenBurger.jpg"><img src="img/assets/fastfood/beefBurger.jpg"><img src="img/assets/fastfood/chickenPizza.jpg">
+
+</div>
+<div class="jumbotron">
+</div>
+<div class="bottom">
+<h3>Our Happy Customers</h3>
+<subtitle>George Richard</subtitle>
+<p>Geat food!Menu is awesome,there foods are not only great but also in the right price.I'll highly recommend them:)</p>
+<subtitle>About Menu</subtitle>
+<p>We are menu a little team of 7members.Ut wisi enim ad minim veniam,quis nos trudexer ci tation ullamcorper suscipit lob ortis nisl ut aliquip ex ea commodo conse quat.Duisau tem vel eum iriure dolor in.</p>
+</div>
+<div class="jumbotron">
+</div>
+<div class="mail">
+  <img src="img/assets/backgrounds/subscribe.jpg">
+<br><br><br>
+<div class="container">
+  <h1>Get our daily food items delivered to your email</h1>
+  <p>Click a button to enter your email:</p>
+
+  <button class="btn btn-brown" id="brown">subscribe</button>
+</div>
+</div>
+<div class="footerlogos">
+  <img src="img/assets/footerlogos/logo_white.png"><p>MENU</p>
+  <p>Thats What We Are</p>
+  <img src="img/assets/footerlogos/facebook.png"><img src="img/assets/footerlogos/twitter.png"><img src="img/assets/footerlogos/pinterest.png">
+
+
+
+
+  </body
+
+</html>
+.utra{
+  font-size: 18px;
+}
+.column{
+  column-count: 2 * 2;
+  width: 1400px;
+  padding: 15px;
+}
+subtitle{
+  font-size: 16px;
+  font-weight:bold;
+}
+.bottom{
+  column-count: 2;
+}
+.logoBig{
+  text-align: center;
+}
+.footerlogos{
+  background-color:#f46c36;
+  text-align: center;
+}
+.landing{
+  background-image: url(..img/assets/backgrounds/subscribe.jpg);
+  background-size:cover;
+  height: 636px;
+}
+.table{
+  column-count: 2;
+}
+.utra{
+  font-size: 18px;
+}
+.column{
+  column-count: 2 * 2;
+  width: 1400px;
+  padding: 15px;
+}
+subtitle{
+  font-size: 16px;
+  font-weight:bold;
+}
+.bottom{
+  column-count: 2;
+}
+.logoBig{
+  text-align: center;
+}
+.footerlogos{
+  background-color:#f46c36;
+  text-align: center;
+}
+.landing{
+  background-image: url(..img/assets/backgrounds/subscribe.jpg);
+  background-size:cover;
+  height: 636px;
+}
+.table{
+  column-count: 2;
+}
